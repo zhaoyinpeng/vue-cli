d86ce9b9612867abd52a8770792ce1adce65d445
zhaoyinpeng <18848962908@163.com>
2019-04-24 23:17:22 +0800

    分页功能

---
+++ src/components/paging.vue
@@ -0,0 +1,169 @@
+<!-- 分页插件 -->
+<template>
+  <div class="paging">
+    <span class="totalPage">当前页数：{{totalPage}}</span>
+    <span class="currentPage">当前页数：{{cPage}}</span>
+    <a :class="[cPage === 1?'disableClick':'','prevPage']" @click="prevPage">上一页</a>
+    <a href="javascript:void(0);" v-show="prevPageOmit">...</a>
+    <a
+      href="javascript:void(0);"
+      v-for="item in prevPageList"
+      :key="item"
+      @click="gotoPage(item)"
+    >{{item}}</a>
+    <a href="javascript:void(0);" class="aCurrentPage">{{cPage}}</a>
+    <a
+      href="javascript:void(0);"
+      v-for="item in nextPageList"
+      :key="item"
+      @click="gotoPage(item)"
+    >{{item}}</a>
+    <a href="javascript:void(0);" v-show="nextPageOmit">...</a>
+    <a :class="[cPage === totalPage?'disableClick':'','nextPage']" @click="nextPage">下一页</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "paging",
+  components: {},
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    totalPage: {
+      type: Number,
+      default: 1
+    }
+  },
+  data() {
+    return {
+      cPage: 1,
+      prevPageList: [],
+      nextPageList: [],
+      prevPageOmit: false,
+      nextPageOmit: false,
+      showPageNum: 3
+    };
+  },
+  computed: {},
+  watch: {
+    cPage(newVal, oldVal) {
+      this.cPageChange();
+      this.calcPageList();
+    }
+  },
+  //方法集合
+  methods: {
+    prevPage() {
+      if (this.cPage !== 1) {
+        this.cPage--;
+      }
+    },
+    nextPage() {
+      if (this.cPage !== this.totalPage) {
+        this.cPage++;
+      }
+    },
+    gotoPage(item) {
+      this.cPage = item;
+    },
+    cPageChange() {
+      this.$emit("cPageChange", this.cPage);
+    },
+    calcPageList() {
+      //前半部分 当前页数之前
+      if (this.cPage === 1) {
+        this.prevPageList = [];
+      }
+      if (this.cPage > 1 && this.cPage <= 3) {
+        let array = [...Array(this.cPage).keys()].splice(0, 1);
+        this.prevPageList = array;
+      }
+      if (this.cPage > 3) {
+        let array = [];
+        for (let index = this.cPage - 3; index <= this.cPage - 1; index++) {
+          array.push(index);
+        }
+        this.prevPageList = array;
+      }
+      //是否显示前面省略号
+      if (this.prevPageList.length === 3 && this.prevPageList[0] > 1) {
+        this.prevPageOmit = true;
+      } else {
+        this.prevPageOmit = false;
+      }
+
+      //后半部分
+      if (this.cPage === this.currentPage) {
+        this.nextPageList = [];
+      }
+
+      if (this.cPage < this.totalPage - 3) {
+        let array = [];
+        for (let index = this.cPage + 1; index <= this.cPage + 4; index++) {
+          array.push(index);
+        }
+        this.nextPageList = array;
+      }
+      if (this.cPage < this.totalPage && this.cPage >= this.totalPage - 3) {
+        let array = [];
+        for (let index = this.cPage + 1; index <= this.totalPage; index++) {
+          array.push(index);
+        }
+        this.nextPageList = array;
+      }
+
+      //是否显示后面省略号
+      if (
+        this.nextPageList.length === 3 &&
+        this.nextPageList[2] < this.totalPage
+      ) {
+        this.nextPageOmit = true;
+      } else {
+        this.nextPageOmit = false;
+      }
+    }
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    this.cPage = this.currentPage;
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {}
+};
+</script>
+<style lang='less' scoped>
+.paging {
+  display: flex;
+  flex-direction: row;
+  height: 30px;
+  user-select: none;
+  & > a {
+    display: inline-block;
+    height: 100%;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    padding: 0 10px;
+    line-height: 30px;
+    cursor: pointer;
+    box-sizing: border-box;
+    text-decoration: none;
+    color: #333;
+  }
+  & > a.disableClick {
+    cursor: not-allowed;
+    background-color: #ccc;
+  }
+  & > a.aCurrentPage{
+    background-color: brown;
+  }
+  & > span {
+    display: inline-block;
+    height: 100%;
+    padding: 0 10px;
+    line-height: 30px;
+  }
+}
+</style>
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -5,22 +5,29 @@
     <img-component @fromchildren="showMsg" :imgWidth="50" :imgHeight="50"></img-component>
     <img-component :imgWidth="150" :imgHeight="150"></img-component>
     <p>插件</p>
+    <p>全局弹窗</p>
     <button class="btn" @click="msgPlugin('info弹窗','info')">info弹窗</button>
     <button class="btn" @click="msgPlugin('success弹窗','success')">success弹窗</button>
     <button class="btn" @click="msgPlugin('warning弹窗','warning')">warning弹窗</button>
     <button class="btn" @click="msgPlugin('error弹窗','error')">error弹窗</button>
+    <p>分页</p>
+    <paging :currentPage="currentPage" :totalPage="totalPage" @cPageChange="watchPageChange"></paging>
   </div>
 </template>
 <script>
 import imgComponent from "../components/image.vue";
+import paging from "../components/paging.vue";
 export default {
   data() {
     return {
-      aboutMsg: "插件，组件"
+      aboutMsg: "插件，组件",
+      currentPage: 1,
+      totalPage: 10
     };
   },
   components: {
-    imgComponent
+    imgComponent,
+    paging
   },
   methods: {
     showMsg: function(msg) {
@@ -31,6 +38,9 @@ export default {
       this.$message[type](text,()=>{
         alert('回调函数');
       });
+    },
+    watchPageChange(val){
+      console.log(val);
     }
   }
 };
